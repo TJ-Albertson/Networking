88011e3c55c63338a26204af00238a1c334ca6f8
TJ-Albertson <THOMASJOHNNY77@GMAIL.COM>
2023-10-27 01:33:46 -0500

    asdf

--- include/common/packet.h
+++ include/common/packet.h
@@ -1,141 +1,49 @@
 #ifndef PACKET_H
 #define PACKET_H
 
-#include <stdint.h>
-#include <stdio.h>
-#include <assert.h>
-
-#define MaxElements 32
-
-typedef struct Buffer {
-    uint8_t* data;  // pointer to buffer data
-    int size;       // size of buffer data (bytes)
-    int index;      // index of next byte to be read/written
-} Buffer;
-
-
-void WriteBits(Buffer& buffer, uint32_t value)
-{
-    assert(buffer.index + 4 <= buffer.size);
-#ifdef BIG_ENDIAN
-    *((uint32_t*)(buffer.data + buffer.index)) = bswap(value);
-#else // #ifdef BIG_ENDIAN
-    *((uint32_t*)(buffer.data + buffer.index)) = value;
-#endif // #ifdef BIG_ENDIAN
-    buffer.index += 4;
-}
-
-void WriteInteger(Buffer& buffer, uint32_t value)
-{
-    assert(buffer.index + 4 <= buffer.size);
-#ifdef BIG_ENDIAN
-    *((uint32_t*)(buffer.data + buffer.index)) = bswap(value);
-#else // #ifdef BIG_ENDIAN
-    *((uint32_t*)(buffer.data + buffer.index)) = value;
-#endif // #ifdef BIG_ENDIAN
-    buffer.index += 4;
-}
-
-
-
-void WriteShort(Buffer& buffer, uint16_t value) {
-    assert(buffer.index + 4 <= buffer.size);
-#ifdef BIG_ENDIAN
-    *((uint32_t*)(buffer.data + buffer.index)) = bswap(value);
-#else // #ifdef BIG_ENDIAN
-    *((uint16_t*)(buffer.data + buffer.index)) = value;
-#endif // #ifdef BIG_ENDIAN
-    buffer.index += 4;
-}
-
-void WriteChar(Buffer& buffer, uint8_t value) {
-    assert(buffer.index + 4 <= buffer.size);
-#ifdef BIG_ENDIAN
-    *((uint32_t*)(buffer.data + buffer.index)) = bswap(value);
-#else // #ifdef BIG_ENDIAN
-    *((uint8_t*)(buffer.data + buffer.index)) = value;
-#endif // #ifdef BIG_ENDIAN
-    buffer.index += 4;
-}
-
-
-uint32_t ReadInteger(Buffer& buffer)
-{
-    assert(buffer.index + 4 <= buffer.size);
-    uint32_t value;
-#ifdef BIG_ENDIAN
-    value = bswap(*((uint32_t*)(buffer.data + buffer.index)));
-#else // #ifdef BIG_ENDIAN
-    value = *((uint32_t*)(buffer.data + buffer.index));
-#endif // #ifdef BIG_ENDIAN
-    buffer.index += 4;
-    return value;
-}
-
-uint16_t ReadShort(Buffer& buffer) {
-    assert(buffer.index + 4 <= buffer.size);
-    uint16_t value;
-#ifdef BIG_ENDIAN
-    value = bswap(*((uint16_t*)(buffer.data + buffer.index)));
-#else // #ifdef BIG_ENDIAN
-    value = *((uint16_t*)(buffer.data + buffer.index));
-#endif // #ifdef BIG_ENDIAN
-    buffer.index += 4;
-    return value;
-}
-
-uint8_t ReadByte(Buffer& buffer) {
-    assert(buffer.index + 4 <= buffer.size);
-    uint8_t value;
-#ifdef BIG_ENDIAN
-    value = bswap(*((uint8_t*)(buffer.data + buffer.index)));
-#else // #ifdef BIG_ENDIAN
-    value = *((uint8_t*)(buffer.data + buffer.index));
-#endif // #ifdef BIG_ENDIAN
-    buffer.index += 4;
-    return value;
-}
+#include "serializer.h"
 
 struct PacketA {
     int x, y, z;
 
-    void Write(Buffer& buffer)
+    bool ReadSerialize(BitReader& reader)
     {
-        WriteInteger(buffer, x);
-        WriteInteger(buffer, y);
-        WriteInteger(buffer, z);
+        read_serialize_bits(reader, x, 32);
+        read_serialize_bits(reader, y, 32);
+        read_serialize_bits(reader, z, 32);
+        return true;
     }
 
-    void Read(Buffer& buffer)
+    bool WriteSerialize(BitWriter& writer)
     {
-        x = ReadInteger(buffer);
-        y = ReadInteger(buffer);
-        z = ReadInteger(buffer);
+        write_serialize_bits(writer, x, 32);
+        write_serialize_bits(writer, y, 32);
+        write_serialize_bits(writer, z, 32);
+        return true;
     }
 };
 
-void test() {
-    Buffer buffer;
+struct PacketB_Two {
+    int numElements;
+    int elements[MaxElements];
 
-    uint8_t data_buffer[256];
-    buffer.data = data_buffer;
+    bool ReadSerialize(BitReader& reader)
+    {
+        read_serialize_int(reader, numElements, 0, MaxElements);
+        for (int i = 0; i < numElements; ++i) {
+            read_serialize_bits(reader, elements[i], 32);
+        }
+        return true;
+    }
 
-    buffer.size = 256;
-    int index = 0;
-    
-    PacketA packet;
-
-    packet.x = 14;
-    packet.y = 27;
-    packet.z = 75;
-
-    packet.Write(buffer);
-
-    PacketA packet2;
-    
-    packet2.Read(buffer);
-
-}
-
+    bool WriteSerialize(BitWriter& writer)
+    {
+        write_serialize_int(writer, numElements, 0, MaxElements);
+        for (int i = 0; i < numElements; ++i) {
+            write_serialize_bits(writer, elements[i], 32);
+        }
+        return true;
+    }
+};
 
 #endif
---
+++ include/common/serializer.h
@@ -0,0 +1,201 @@
+#ifndef SERIALIZER_H
+#define SERIALIZER_H
+
+#include <assert.h>
+#include <stdint.h>
+
+template <uint32_t x>
+struct PopCount {
+    enum { a = x - ((x >> 1) & 0x55555555),
+        b = (((a >> 2) & 0x33333333) + (a & 0x33333333)),
+        c = (((b >> 4) + b) & 0x0f0f0f0f),
+        d = c + (c >> 8),
+        e = d + (d >> 16),
+        result = e & 0x0000003f };
+};
+
+template <uint32_t x>
+struct Log2 {
+    enum { a = x | (x >> 1),
+        b = a | (a >> 2),
+        c = b | (b >> 4),
+        d = c | (c >> 8),
+        e = d | (d >> 16),
+        f = e >> 1,
+        result = PopCount<f>::result };
+};
+
+template <int64_t min, int64_t max>
+struct BitsRequired {
+    static const uint32_t result = (min == max) ? 0 : (Log2<uint32_t(max - min)>::result + 1);
+};
+
+#define BITS_REQUIRED(min, max) BitsRequired<min, max>::result
+
+const int MaxElements = 32;
+const int MaxElementBits = BITS_REQUIRED(0, MaxElements);
+
+typedef struct BitWriter {
+    uint64_t scratch;
+    int scratch_bits;
+    int word_index;
+    uint32_t* buffer;
+} BitWriter;
+
+typedef struct BitReader {
+    uint64_t scratch;
+    int scratch_bits;
+    int total_bits;
+    int total_bits_read;
+    int word_index;
+    uint32_t* buffer;
+} BitReader;
+
+void WriteBits(BitWriter& bitwriter, uint32_t value, int bits)
+{
+    bitwriter.scratch |= (uint64_t)value << bitwriter.scratch_bits;
+    bitwriter.scratch_bits += bits;
+
+    while (bitwriter.scratch_bits >= 32) {
+        bitwriter.buffer[bitwriter.word_index++] = (uint32_t)bitwriter.scratch;
+        bitwriter.scratch >>= 32;
+        bitwriter.scratch_bits -= 32;
+    }
+}
+
+// Function to flush any remaining bits to memory
+void FlushBitsToMemory(BitWriter& bitwriter)
+{
+    if (bitwriter.scratch_bits > 0) {
+        // Flush lower 32 bits to memory
+        bitwriter.buffer[bitwriter.word_index++] = (uint32_t)(bitwriter.scratch & 0xFFFFFFFF);
+    }
+}
+
+uint32_t ReadBits(BitReader& reader, int num_bits_to_read)
+{
+    uint32_t result = 0;
+
+    while (num_bits_to_read > 0) {
+        if (reader.scratch_bits == 0 || num_bits_to_read > reader.scratch_bits) {
+            // Read the next word into scratch
+            uint32_t word = reader.buffer[reader.word_index];
+            reader.scratch |= (uint64_t)word << reader.scratch_bits;
+            reader.scratch_bits += 32;
+            reader.word_index++;
+        }
+
+        // Calculate the number of bits to read from scratch
+        int bits_to_read = (num_bits_to_read < reader.scratch_bits) ? num_bits_to_read : reader.scratch_bits;
+
+        // Extract the bits from scratch
+        uint32_t extracted_bits = reader.scratch & ((1ULL << bits_to_read) - 1);
+
+        // Update scratch and scratch_bits
+        reader.scratch >>= bits_to_read;
+        reader.scratch_bits -= bits_to_read;
+
+        // Update the result with the extracted bits
+        result |= (extracted_bits << (num_bits_to_read - bits_to_read));
+
+        // Update the number of bits left to read
+        num_bits_to_read -= bits_to_read;
+    }
+
+    reader.total_bits_read += num_bits_to_read;
+    return result;
+}
+
+bool ReadOverflow(BitReader bitreader, int bits)
+{
+    return (bitreader.total_bits_read + bits) > bitreader.total_bits;
+}
+
+bool ReadSerializeBits(BitReader& reader, int32_t& value, int bits)
+{
+    assert(bits > 0);
+
+    if (ReadOverflow(reader, bits))
+        return false;
+
+    value = ReadBits(reader, bits);
+
+    return true;
+}
+
+bool WriteSerializeBits(BitWriter& writer, int32_t value, int bits)
+{
+    assert(bits > 0);
+    assert(value >= 0);
+
+    WriteBits(writer, value, bits);
+    return true;
+}
+
+#define read_serialize_bits(reader, value, bits)   \
+    if (!ReadSerializeBits(reader, value, bits)) { \
+        return false;                              \
+    }
+
+#define write_serialize_bits(reader, value, bits)   \
+    if (!WriteSerializeBits(reader, value, bits)) { \
+        return false;                               \
+    }
+
+bool ReadSerializeInteger(BitReader& reader, int32_t& value, int32_t min, int32_t max)
+{
+    assert(min < max);
+
+    const int bits = BITS_REQUIRED(0, MaxElements);
+
+    if (ReadOverflow(reader, bits)) {
+        return false;
+    }
+
+    uint32_t unsigned_value = ReadBits(reader, bits);
+    value = (int32_t)unsigned_value + min;
+    return true;
+}
+
+#define read_serialize_int(reader, value, min, max)                 \
+    do {                                                            \
+        assert(min < max);                                          \
+        int32_t int32_value;                                        \
+        if (!ReadSerializeInteger(reader, int32_value, min, max)) { \
+            return false;                                           \
+        }                                                           \
+        value = int32_value;                                        \
+        if (value < min || value > max) {                           \
+            return false;                                           \
+        }                                                           \
+    } while (0)
+
+bool WriteSerializeInteger(BitWriter& writer, int32_t value, int32_t min, int32_t max)
+{
+    assert(min < max);
+    assert(value >= min);
+    assert(value <= max);
+
+    const int bits = BITS_REQUIRED(0, MaxElements);
+    uint32_t unsigned_value = value - min;
+
+    WriteBits(writer, unsigned_value, bits);
+
+    return true;
+}
+
+#define write_serialize_int(writer, value, min, max)                 \
+    do {                                                             \
+        assert(min < max);                                           \
+        int32_t int32_value;                                         \
+                                                                     \
+        assert(value >= min);                                        \
+        assert(value <= max);                                        \
+        int32_value = (int32_t)value;                                \
+                                                                     \
+        if (!WriteSerializeInteger(writer, int32_value, min, max)) { \
+            return false;                                            \
+        }                                                            \
+    } while (0)
+
+#endif
